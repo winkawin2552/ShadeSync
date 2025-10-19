edcce2b68fb83f83190229763d09ceadae197d6d
winkawin2552 <winkawin2552@gmail.com>
2025-10-19 20:46:16 +0700

    fix vite.config

--- shadesync/vite.config.js
+++ shadesync/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // fix here
+  server: {
+    host: "0.0.0.0", // or use 'true'
+  },
+});
