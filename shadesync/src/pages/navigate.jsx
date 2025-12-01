44bafa8c9ff59c745cb007546ac45ee1a59fd88e
Nrappys <keskarn.parn@gmail.com>
2025-12-01 19:51:45 +0700

    2025.12.1

--- shadesync/src/pages/navigate.jsx
+++ shadesync/src/pages/navigate.jsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import Map from "../components/Map";
 import styles from "./navigate.module.css";
 import glass from "../assets/glass.png";
+import start from "../assets/start.png";
 
 function Navigate() {
   const [points, setPoints] = useState([]);
   const [flyToCoords, setFlyToCoords] = useState(null);
   const [userLocation, setUserLocation] = useState(null);
   const [searchValue, setSearchValue] = useState("");
+
+  const [results, setResults] = useState([
+    { id: 1, time: "19 min",dis: "(3km)", shade: "35%" },
+    { id: 2, time: "15 min",dis: "(2.5km)", shade: "10%" },
+    { id: 3, time: "20 min",dis: "(3.2km)", shade: "30%" },
+  ]);
 
   const handleClear = () => {
     setPoints([]);
@@ -36,13 +43,16 @@ function Navigate() {
   const handleSearch = (e) => {
     e.preventDefault();
     console.log("Searching for:", searchValue);
-    // Add search functionality here
   };
 
   return (
     <div style={{ width: "100%", minHeight: "1400px", backgroundColor: "#ffffffff" }}>
-      <h1 style={{color: "#595959",textAlign: "center",fontFamily: "agdasima bold, monospace",}}>Shade Navigation </h1>
-      <h5 style={{color: "#595959",textAlign: "center",fontWeight: "500",}}> Go everywhere with shade</h5>
+      <h1 style={{ color: "#595959", textAlign: "center", fontFamily: "agdasima bold, monospace" }}>
+        Shade Navigation
+      </h1>
+      <h5 style={{ color: "#595959", textAlign: "center", fontWeight: "500" }}>
+        Go everywhere with shade
+      </h5>
 
       <form onSubmit={handleSearch} className={styles.searchContainer}>
         <input
@@ -52,7 +62,7 @@ function Navigate() {
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
         />
-        <img src={glass} className={styles.glass}/>
+        <img src={glass} className={styles.glass} />
       </form>
 
       <div className={styles.mapContainer}>
@@ -63,27 +73,52 @@ function Navigate() {
           userLocation={userLocation}
           onGoToLocation={handleGoToLocation}
         />
+
         <div style={{ display: "flex", gap: "10px" }}>
           <button className={styles.confirm}>
             <h4>Confirm Location</h4>
           </button>
+
           <button onClick={handleClear} className={styles.clearButton}>
             X
           </button>
         </div>
       </div>
-    
-      <h1 style={{paddingTop: "100px",paddingLeft: "17px",fontFamily: 'agdasima bold, monospace', fontSize: "2rem", fontWeight: 600, color: "#595959", letterSpacing: "1px"}}>Result</h1>
+
+      <h1
+        style={{
+          paddingTop: "100px",
+          paddingLeft: "17px",
+          fontFamily: "agdasima bold, monospace",
+          fontSize: "2rem",
+          fontWeight: 600,
+          color: "#595959",
+          letterSpacing: "1px",
+        }}
+      >
+        Result
+      </h1>
       <div className={styles.resultcontainer}>
-        <div className={styles.result1}>
-          
-        </div>
-        <div className={styles.result1}>
-
-        </div>
-        <div className={styles.result1}>
-
-        </div>
+        {results.map((item) => (
+          <div key={item.id} className={styles.result1}>
+            <div className={styles.timeContainer}>
+              <h2 style={{paddingLeft: "17px",fontFamily: "agdasima bold, monospace",color:'#2BBB4A'}}>{item.time}</h2>
+              <h2 style={{paddingLeft: "10px",fontFamily: "agdasima bold, monospace",color:'#000000ff'}}>{item.dis}</h2>
+              
+            </div>
+            <div className={styles.shadeContainer}>
+              <h2 style={{paddingLeft: "17px",fontFamily: "agdasima bold, monospace",color:'#2BBB4A'}}>Shade</h2>
+              <h2 style={{paddingLeft: "10px",fontFamily: "agdasima bold, monospace",color:'#000000ff'}}>{item.shade}</h2>
+            </div>
+            <button className={styles.delete}>
+                X
+            </button>
+            <button class={styles.startbut}>
+              <img src={start} className={styles.start}/>
+              <span>Start</span>
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
